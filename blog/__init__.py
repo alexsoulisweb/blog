0c3a1cdb621860940a330f8e3a22d270f5cae5e8
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-06-17 16:30:42 +0200

    Update __init__.py

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,4 +1,3 @@
-# puppycompanyblog/__init__.py
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -11,9 +10,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
 
-############################
-### DATABASE SETUP ##########
-########################
+
+#DATABASE SETUP 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,16 +19,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 
-#########################
 # LOGIN CONFIGS
 login_manager = LoginManager()
 
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
-
-
-
-##################################################
 
 
 from blog.core.views import core
